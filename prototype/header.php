cf62f5b6071f5f0cba32d183b91c89e1df9e5008
Zochoco <71255113+zocheese@users.noreply.github.com>
2020-09-18 11:34:30 +1000

    Revert "Web development"

--- prototype/header.php
+++
@@ -1,9 +0,0 @@
-<?php 
-echo '<li><a href="#profile"><img class="profile" src="prophoto.png"></a></li>
-<li><a href="#notes">Alerts</a></li>  <!-- TODO -->
-<li><a href="#notes">Notes</a></li>
-<li><a href="#dictionary">Dictionary</a></li>
-<li><a href="#forums">Forums</a></li>
-<li><a href="#flashcards">Flashcards</a></li>
-<li><a href="dashboard.html">Dashboard</a></li>'
-?>
